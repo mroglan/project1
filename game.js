525bdcea1f94e209f454cfa640042f92c7e017e3
mroglan <59785089+mroglan@users.noreply.github.com>
2020-02-14 11:09:27 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,11 @@
+let inputValue, flexDiv, image1;
 
+window.onload = function() {
+  
+  inputValue = parseInt(document.querySelector("#inputValue").value);
+  flexDiv = document.querySelector("flexContainer");
+};
 
-
+inputValue.onchange = function() {
+  
 
